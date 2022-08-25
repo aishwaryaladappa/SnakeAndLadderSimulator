dba18b7c4b43b6535db7288bbc89548419383afc
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-08-25 15:03:33 +0530

    added RollsDie.cs

---
+++ RollsDie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderSimulator
+{
+    public static class RollsDie
+    {       
+        public static void DisplayRandom()
+        {
+            Random random = new Random();
+            int number = random.Next(1, 6);
+            Console.WriteLine("random number is:" + number);
+        }
+
+    }
+}
